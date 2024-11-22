5164f6dbd722583b07b3b00097fd6b84eeacb4e7
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2024-11-22 23:06:08 +0100

    EX7

---
+++ ex7.js
@@ -0,0 +1,3 @@
+//ex7 Crea una estructura Map i insereix 3 claus – valor. Les 3 claus han de ser de tipus de dades diferents. Et deixa?
+
+let mapa = new Map([['a',"bondia"],['b',1],['c',true],['d','A']]);

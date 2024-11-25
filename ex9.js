3328a7d17f0356c754ba21bf179f827958da7ed0
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2024-11-25 08:38:06 +0100

    EX3

--- ex9.js
+++ ex9.js
@@ -1,6 +1,6 @@
 //ex9 Crea una Cua d’Objectes amb dades de 3 pel·lícules. Itera la cua per mostrar per pantalla la pel·lícula més antiga.
 
-let pelicules = [{nom:"pelicula",desc:"descripcio1",any: 1998},{nom:"pelicula2",desc:"descripcio2",any:2030},{nom:"pelicula3",desc:"descripcio3",any:1027}]
+let pelicules = [{nom:"pelicula",desc:"descripcio1",any: 1998},{nom:"pelicula2",desc:"descripcio2",any:2030},{nom:"pelicula3",desc:"descripcio3",any:1027},{nom:"pelicula4",desc:"descripcio4",any:990}]
 let petit = 99999;
 let posicioP = 0;
 

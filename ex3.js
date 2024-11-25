073c8f300f62270656869eeaf31f3a99299b5cb0
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2024-11-25 08:37:55 +0100

    EX3

--- ex3.js
+++ ex3.js
@@ -1,10 +1,24 @@
 //ex3 Fusiona dos arrays i esborren els valors duplicats (no utilitzis el Set).
 
 let array1 = [1,2,3,4,5];
-let array2 = [6,7,8,9,10];
-let array3 = [];
+let array2 = [6,2,8,4,10];
+let canvi = [...array1, ...array2];
+let cont = 0;
+let array3 = []
 
-array3 = [...array1,...array2]
+for (let i = 0; i < canvi.length; i++) {
+    for (let j = i+1; j < canvi.length; j++) {
+        if (canvi[i] === canvi[j]) {
+            canvi[i] = 9999999;
+        }
+    }
+}
+
+for (let j = 0; j < canvi.length; j++) {
+    if (canvi[j] !== 9999999){
+        array3.push(canvi[j]);
+    }
+}
 
 console.log("array1");
 for(let num in array1) {
